926d2a40f65e7391bbd4c4f56c3d7e14262f54b7
xamarsia <xamarsia@gmail.com>
2025-07-11 15:52:41 -0400

    Add common-icons package

---
+++ packages/common-icons/src/chevron-up.tsx
@@ -0,0 +1,11 @@
+type IconProps = {
+    style?: string,
+}
+
+export default function ChevronUp({ style }: IconProps) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className={`${style}`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+        </svg>
+    )
+}
---
+++ packages/common-icons/src/minus.tsx
@@ -0,0 +1,11 @@
+type IconProps = {
+    style?: string,
+}
+
+export default function MinusIcon({ style }: IconProps) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${style}`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+        </svg>
+    )
+}
