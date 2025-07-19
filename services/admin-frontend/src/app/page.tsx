f71f41de1e4ca085eabacc8ad3a2dcd37edff3e7
xamarsia <xamarsia@gmail.com>
2025-07-19 19:10:47 -0400

    [SEL-37] Add admin UI components package

--- services/admin-frontend/src/app/page.tsx
+++ services/admin-frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import AddAmountButton from "@sellify/admin-ui-components/AddAmountButton";
 import Button from "@sellify/common-ui-components/buttons/Button";
 
+import { useState } from "react";
 
 export default function Home() {
+  const [quantity, setQuantity] = useState<number>();
+
   return (
     <div className="flex-row gap-20">
       <div className="flex m-16 gap-10">
@@ -16,6 +20,19 @@ export default function Home() {
         <Button size="small" variant="outline">
           <p> Small Button</p>
         </Button>
+      </div>
+      <div className="flex m-16 gap-10">
+        <AddAmountButton
+          value={quantity}
+          onChange={setQuantity}
+          onSubmit={setQuantity}
+        />
+        <AddAmountButton
+          value={quantity}
+          onChange={setQuantity}
+          onSubmit={setQuantity}
+          disabled
+        />
       </div>
     </div>
   );
