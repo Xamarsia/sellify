443a2df8a88d6823e5569b1c682c37d44470d531
xamarsia <xamarsia@gmail.com>
2025-09-05 02:32:14 -0400

    Format common-ui-components-example

--- examples/common-ui-components-example/src/examples/AlertDialogExample.tsx
+++ examples/common-ui-components-example/src/examples/AlertDialogExample.tsx
@@ -1,50 +1,49 @@
 "use client";
 
-
 import { useCallback, useState } from "react";
 
 import Button from "@sellify/common-ui-components/buttons/Button";
 import AlertDialog from "@sellify/common-ui-components/dialog/AlertDialog";
 import FireIcon from "@sellify/common-icons/fire";
 
-
 type DialogProps = {
-    title: string,
-}
+  title: string;
+};
 
 export default function AlertDialogExample({ title }: DialogProps) {
-    const [opened, setOpened] = useState<boolean>(false);
+  const [opened, setOpened] = useState<boolean>(false);
 
+  const onCloseClicked = useCallback((): void => {
+    setOpened(false);
+  }, []);
 
-    const onCloseClicked = useCallback((): void => {
-        setOpened(false);
-    }, []);
+  const onOpenDialog = useCallback(() => {
+    setOpened(!opened);
+  }, [opened]);
 
-    const onOpenDialog = useCallback(() => {
-        setOpened(!opened);
-    }, [opened]);
-
-
-    return (
-        <>
-            <Button onClick={onOpenDialog}>
-                Open Alert Dialog
+  return (
+    <>
+      <Button onClick={onOpenDialog}>Open Alert Dialog</Button>
+      <AlertDialog
+        title={title}
+        dialogOpen={opened}
+        onDialogClose={onCloseClicked}
+        icon={<FireIcon />}
+      >
+        <div className="grow flex flex-col justify-between gap-4">
+          <div className="grow flex flex-col gap-4 shrink-0">
+            <div className="h-2.5 bg-gray-200 rounded-full " />
+          </div>
+          <div className="flex justify-between ">
+            <Button variant="outline">
+              <div className="h-2.5 my-3 bg-gray-200 rounded-full w-32" />
             </Button>
-            <AlertDialog title={title} dialogOpen={opened} onDialogClose={onCloseClicked} icon={<FireIcon />}>
-                <div className="grow flex flex-col justify-between gap-4">
-                    <div className="grow flex flex-col gap-4 shrink-0">
-                        <div className="h-2.5 bg-gray-200 rounded-full " />
-                    </div>
-                    <div className="flex justify-between ">
-                        <Button variant="outline">
-                            <div className="h-2.5 my-3 bg-gray-200 rounded-full w-32" />
-                        </Button>
-                        <Button>
-                            <div className="h-2.5 my-3 bg-gray-200 rounded-full w-32" />
-                        </Button>
-                    </div>
-                </div>
-            </AlertDialog>
-        </>
-    )
+            <Button>
+              <div className="h-2.5 my-3 bg-gray-200 rounded-full w-32" />
+            </Button>
+          </div>
+        </div>
+      </AlertDialog>
+    </>
+  );
 }
--- examples/common-ui-components-example/src/examples/SearchBarExample.tsx
+++ examples/common-ui-components-example/src/examples/SearchBarExample.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 
 import { useRouter } from "next/navigation";
@@ -8,25 +7,31 @@ import { useRouter } from "next/navigation";
 import SearchInput from "@sellify/common-ui-components/input/SearchInput";
 
 export default function SearchBarExample() {
-    const [query, setQuery] = useState<string>('');
-    const router = useRouter();
+  const [query, setQuery] = useState<string>("");
+  const router = useRouter();
 
-    const onSubmit = useCallback((e: FormEvent<HTMLFormElement>): void => {
-        e.preventDefault();
-        if (query) {
-            router.push(`/search/?query=${query}`);
-        }
-    }, [query, router]);
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>): void => {
+      e.preventDefault();
+      if (query) {
+        router.push(`/search/?query=${query}`);
+      }
+    },
+    [query, router],
+  );
 
-    const onSearchChanged = useCallback((e: ChangeEvent<HTMLInputElement>): void => {
-        e.preventDefault();
-        const query: string = e.target.value;
-        setQuery(query);
-    }, []);
+  const onSearchChanged = useCallback(
+    (e: ChangeEvent<HTMLInputElement>): void => {
+      e.preventDefault();
+      const query: string = e.target.value;
+      setQuery(query);
+    },
+    [],
+  );
 
-    return (
-        <form onSubmit={onSubmit}>
-            <SearchInput value={query} onChange={onSearchChanged} />
-        </form>
-    )
+  return (
+    <form onSubmit={onSubmit}>
+      <SearchInput value={query} onChange={onSearchChanged} />
+    </form>
+  );
 }
