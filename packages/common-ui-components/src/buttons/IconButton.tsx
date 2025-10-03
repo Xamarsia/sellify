957308fb31fa7d8c900bcf586639930ea457c84a
xamarsia <xamarsia@gmail.com>
2025-10-03 00:52:34 -0400

    fix buttons

--- packages/common-ui-components/src/buttons/IconButton.tsx
+++ packages/common-ui-components/src/buttons/IconButton.tsx
@@ -19,9 +19,9 @@ export default function IconButton({
 }: IconButtonProps) {
   const variantStyle = {
     default:
-      "text-white bg-[#383838] enabled:hover:bg-[#242424] focus:bg-[#383838] disabled:bg-disabled",
+      "text-white bg-[#383838] enabled:hover:bg-[#242424] active:bg-[#383838] disabled:bg-disabled",
     outline:
-      "text-black bg-primary border border-stroke enabled:hover:border-black focus:border-black disabled:text-disabled",
+      "text-black bg-primary border border-stroke enabled:hover:border-black active:border-black disabled:text-disabled",
   }[variant];
 
   return (
@@ -29,7 +29,7 @@ export default function IconButton({
       type={type}
       className={`flex justify-center items-center rounded-lg enabled:cursor-pointer disabled:cursor-not-allowed
                     body size-10 min-w-10
-                    ring-stroke focus:ring-4
+                    ring-stroke active:ring-4
                     ${variantStyle}
             `}
       disabled={disabled}
