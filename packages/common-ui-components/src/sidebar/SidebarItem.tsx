2648942636767990355411d450642415d1424232
xamarsia <xamarsia@gmail.com>
2025-10-10 02:20:35 -0400

    fix

--- packages/common-ui-components/src/sidebar/SidebarItem.tsx
+++ packages/common-ui-components/src/sidebar/SidebarItem.tsx
@@ -14,9 +14,9 @@ export default function SidebarItem({
   return (
     <a
       href={href}
-      className={`flex items-center px-6 h-13 bg-primary underline-offset-4 
-        enabled:cursor-pointer  hover:text-[#000000] 
-        body ${selected ? `underline text-[#000000]` : `text-[#555555]`}
+      className={`flex items-center h-13 bg-primary underline-offset-4 
+        hover:underline hover:text-black enabled:cursor-pointer body
+        ${selected ? `underline text-black` : `text-[#555555]`}
       `}
     >
       {text}
