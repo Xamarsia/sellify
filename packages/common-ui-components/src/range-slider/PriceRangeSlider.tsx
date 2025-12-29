d76c04c4b86ab1568cd4061c4051eb269e27b800
Ivanka A. <67323429+Xamarsia@users.noreply.github.com>
2025-12-28 23:07:23 -0500

    [SEL-99] Add input fields to range slider component

--- packages/common-ui-components/src/range-slider/PriceRangeSlider.tsx
+++ packages/common-ui-components/src/range-slider/PriceRangeSlider.tsx
@@ -25,17 +25,11 @@ export default function PriceRangeSlider({ range }: Props) {
   );
 
   return (
-    <div className="w-full flex items-center justify-center flex-col gap-6">
-      <div className="w-full flex items-center justify-between gap-x-5">
-        <p> $ {currentRange.min} </p>
-        <p> $ {currentRange.max} </p>
-      </div>
-      <RangeSlider
-        range={range}
-        currentRange={currentRange}
-        onMinValueChange={onMinValueChange}
-        onMaxValueChange={onMaxValueChange}
-      />
-    </div>
+    <RangeSlider
+      range={range}
+      currentRange={currentRange}
+      onMinValueChange={onMinValueChange}
+      onMaxValueChange={onMaxValueChange}
+    />
   );
 }
--- packages/common-ui-components/src/range-slider/RangeSlider.tsx
+++ packages/common-ui-components/src/range-slider/RangeSlider.tsx
@@ -7,6 +7,7 @@ import React, {
 } from "react";
 import RangeSliderInput from "./RangeSliderInput";
 import { SliderRange } from "../types";
+import NumberInput from "./NumberInput";
 
 type Props = {
   range: SliderRange;
@@ -42,7 +43,7 @@ export default function RangeSlider({
     console.log("min: " + currentRange.min, "max: " + currentRange.max);
   }, [currentRange]);
 
-  // set width of the range to decrease from the left side
+  // Set range width to decrease from the left
   useEffect(() => {
     if (selectedRange.current) {
       selectedRange.current.style.left = `${minPercent}%`;
@@ -50,7 +51,7 @@ export default function RangeSlider({
     }
   }, [currentRange.min]);
 
-  // set the width of the range to decrease from right side
+  // Set range width to decrease from the right
   useEffect(() => {
     if (selectedRange.current) {
       selectedRange.current.style.width = `${maxPercent - minPercent}%`;
@@ -81,7 +82,24 @@ export default function RangeSlider({
 
   return (
     <>
-      <div className="relative w-full flex items-center justify-center flex-col space-y-14">
+      <div className="relative w-full flex items-center justify-center flex-col gap-6">
+        <div className="w-full flex items-center justify-between gap-x-10">
+          <NumberInput
+            min={range.min}
+            max={currentRange.max - 1}
+            value={currentRange.min}
+            title="From"
+            onChange={onMinValueChange}
+          />
+          <NumberInput
+            min={currentRange.min + 1}
+            max={range.max}
+            value={currentRange.max}
+            title="To"
+            onChange={onMaxValueChange}
+          />
+        </div>
+
         <div className="multi-slide-input-container w-full">
           <RangeSliderInput
             min={range.min}
@@ -89,8 +107,7 @@ export default function RangeSlider({
             value={currentRange.min}
             onChange={handleMinValueChange}
             style={
-              currentRange.min > range.max - 100 ||
-                currentRange.min === currentRange.max
+              currentRange.min > range.max - 100
                 ? "z-5"
                 : "z-3"
             }
