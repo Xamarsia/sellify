83c8948d64bb4208f7f0f64dfce12ac3f00245af
xamarsia <xamarsia@gmail.com>
2026-01-03 18:28:14 -0500

    [SEL-107] Add inventory page to admin-frontend service

--- packages/admin-ui-components/src/data-view/AddAmountButtonTableItem.tsx
+++ packages/admin-ui-components/src/data-view/AddAmountButtonTableItem.tsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import AddAmountButton from "../AddAmountButton";
 
 type Props = {
-  onSubmit: (value: number) => void;
+  productId: number;
+  onSubmit: (productId: number, quantity: number) => void;
   disabled?: boolean;
 };
 
 export default function AddAmountButtonTableItem({
+  productId,
   disabled,
   onSubmit,
 }: Props) {
   const [quantity, setQuantity] = useState<number>();
 
+  const onAmountChange = useCallback((): void => {
+    if (quantity) {
+      onSubmit(productId, quantity);
+    }
+  }, [quantity, productId, onSubmit]);
+
   return (
     <AddAmountButton
       value={quantity}
       onChange={setQuantity}
-      onSubmit={onSubmit}
+      onSubmit={onAmountChange}
       disabled={disabled}
     />
   );
