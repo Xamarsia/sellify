784776922fe043af3b8dc198673b732689868e10
xamarsia <xamarsia@gmail.com>
2026-01-03 01:58:09 -0500

    [SEL-74] Add product page to admin-frontend service

---
+++ services/admin-frontend/src/app/products/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { ChangeEvent, useCallback, useState } from "react";
+
+import Button from "@sellify/common-ui-components/buttons/Button";
+import SearchInput from "@sellify/common-ui-components/input/SearchInput";
+import Dropdown from "@sellify/common-ui-components/dropdown/Dropdown";
+
+import { Product } from "@sellify/admin-ui-components/types";
+import ProductsView from "@sellify/admin-ui-components/data-view/ProductsView";
+import { filterProducts, getProducts } from "common/actions/product-actions";
+
+import PageTitle from "components/PageTitle";
+
+export default function ProductsPage() {
+  const products: Array<Product> = getProducts();
+  const [query, setQuery] = useState<string>("");
+  const [sortByKey, setSortByKey] = useState<string>();
+  const [currentProducts, setCurrentProducts] =
+    useState<Array<Product>>(products);
+
+  const comboboxSortItems = new Map<string, string>([
+    ["newest", "Rank by newest date"],
+    ["oldest", "Rank by oldest date"],
+    ["status", "Rank by status"],
+    ["byCategory", "Rank by category"],
+    ["byAmount", "Rank by amount"],
+    ["byLowestPrice", "Rank by lowest price"],
+    ["byHighestPrice", "Rank by highest price"],
+  ]);
+
+  const onSearchChanged = useCallback(
+    (e: ChangeEvent<HTMLInputElement>): void => {
+      e.preventDefault();
+      const query: string = e.target.value;
+      setQuery(query);
+      setCurrentProducts(query ? filterProducts(query) : products);
+    },
+    [],
+  );
+
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <PageTitle />
+        <Button size="small">Add Product</Button>
+      </div>
+
+      <div className="flex flex-col w-full gap-4">
+        <div className="relative flex w-full justify-between items-start gap-4">
+          <SearchInput value={query} onChange={onSearchChanged} />
+          <Dropdown
+            title={"sort by"}
+            items={comboboxSortItems}
+            selectedKey={sortByKey}
+            onKeySelected={setSortByKey}
+          />
+        </div>
+        <ProductsView content={currentProducts} />
+      </div>
+    </>
+  );
+}
--- services/admin-frontend/src/common/actions/product-actions.tsx
+++ services/admin-frontend/src/common/actions/product-actions.tsx
@@ -1,4 +1,4 @@
-import { ProductPreview } from "@sellify/admin-ui-components/types";
+import { Product, ProductPreview } from "@sellify/admin-ui-components/types";
 
 import image from "resources/1/image.jpg";
 import image3 from "resources/2/image.jpg";
@@ -27,6 +27,34 @@ const productPreview3: ProductPreview = {
     "LongUnbreakableProductTitleWord|LongUnbreakableProductTitleWordLongUnbreakableProductTitleWord",
 };
 
+const product: Product = {
+  productId: 12323,
+  image: image.src,
+  status: "ACTIVE",
+  title: "Product Title",
+  shortDescription:
+    "Short Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus, massa venenatis ornare aliquam, urna enim interdum nibh, non fermentum magna odio eget odio. Vivamus a egestas nulla. Donec ac ornare nunc, ut tristique risus. Nam faucibus imperdiet ante ut laoreet. Proin mollis pellentesque dictum. Integer sagittis nec mi sed dignissim. Curabitur consectetur elementum ligula, a vulputate arcu elementum quis. Nulla vehicula consectetur mauris vitae fringilla. Aliquam erat volutpat. Duis aliquam purus eget finibus bibendum. Proin viverra elit mauris, quis iaculis ex egestas bibendum. Proin hendrerit ligula id ipsum blandit, efficitur tempus est faucibus. In convallis tortor vitae aliquet fringilla. Morbi hendrerit efficitur urna, et porta sapien blandit nec.",
+  description:
+    "Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus, massa venenatis ornare aliquam, urna enim interdum nibh, non fermentum magna odio eget odio. Vivamus a egestas nulla. Donec ac ornare nunc, ut tristique risus. Nam faucibus imperdiet ante ut laoreet. Proin mollis pellentesque dictum. Integer sagittis nec mi sed dignissim. Curabitur consectetur elementum ligula, a vulputate arcu elementum quis. Nulla vehicula consectetur mauris vitae fringilla. Aliquam erat volutpat. Duis aliquam purus eget finibus bibendum. Proin viverra elit mauris, quis iaculis ex egestas bibendum. Proin hendrerit ligula id ipsum blandit, efficitur tempus est faucibus. In convallis tortor vitae aliquet fringilla. Morbi hendrerit efficitur urna, et porta sapien blandit nec.",
+  quantity: 0,
+  category: "flowers",
+  price: 443,
+};
+
+const product2: Product = {
+  productId: 2344,
+  image: image.src,
+  status: "ARCHIVED",
+  title:
+    "Long Product Title | Long Product Title | Long Product Title | Long Product Title | Long Product Title | Long Product Title | Long Product Title | Long Product Title | Long Product Title",
+  shortDescription: "Short description | Short description | Short description",
+  description:
+    "Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus, massa venenatis ornare aliquam, urna enim interdum nibh, non fermentum magna odio eget odio. Vivamus a egestas nulla. Donec ac ornare nunc, ut tristique risus. Nam faucibus imperdiet ante ut laoreet. Proin mollis pellentesque dictum. Integer sagittis nec mi sed dignissim. Curabitur consectetur elementum ligula, a vulputate arcu elementum quis. Nulla vehicula consectetur mauris vitae fringilla. Aliquam erat volutpat. Duis aliquam purus eget finibus bibendum. Proin viverra elit mauris, quis iaculis ex egestas bibendum. Proin hendrerit ligula id ipsum blandit, efficitur tempus est faucibus. In convallis tortor vitae aliquet fringilla. Morbi hendrerit efficitur urna, et porta sapien blandit nec.",
+  quantity: 23,
+  category: "flowers",
+  price: 443,
+};
+
 export function getProductPreviews(): Array<ProductPreview> {
   const previews: Array<ProductPreview> = [
     productPreview,
@@ -49,4 +77,23 @@ export function getProductPreviews(): Array<ProductPreview> {
     productPreview3,
   ];
   return previews;
+}
+
+export function getProducts(): Array<Product> {
+  const previews: Array<Product> = [
+    product,
+    product2,
+    product,
+    product,
+    product,
+    product2,
+    product2,
+    product2,
+    product,
+  ];
+  return previews;
+}
+
+export function filterProducts(query: string): Array<Product> {
+  return [product, product2];
 }
