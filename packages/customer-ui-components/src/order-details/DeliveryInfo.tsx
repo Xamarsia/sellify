7f4fb62f12b3cd641105786719124733e0cab364
Ivanka A. <67323429+Xamarsia@users.noreply.github.com>
2026-01-06 14:02:36 -0500

    [SEL-130] Fix FormSection component view

--- packages/customer-ui-components/src/order-details/DeliveryInfo.tsx
+++ packages/customer-ui-components/src/order-details/DeliveryInfo.tsx
@@ -6,10 +6,10 @@ type DeliveryInfoProps = {
 
 export default function DeliveryInfo({ order }: DeliveryInfoProps) {
   return (
-    <div className="flex flex-col w-full gap-4">
+    <>
       <p>{`Tracking ID: ${order.trackingDeliveryId}`}</p>
       <p>{`Estimated delivery date: ${order.deliveryDate}`}</p>
       <p>{`Delivery charge: $${order.deliveryFee}`}</p>
-    </div>
+    </>
   );
 }
--- packages/customer-ui-components/src/order-details/PaymentInfo.tsx
+++ packages/customer-ui-components/src/order-details/PaymentInfo.tsx
@@ -6,8 +6,8 @@ type PaymentInfoProps = {
 
 export default function PaymentInfo({ paymentMethodInfo }: PaymentInfoProps) {
   return (
-    <div className="flex flex-col w-full gap-4">
+    <>
       <p> {paymentMethodInfo.title} </p>
-    </div>
+    </>
   );
 }
