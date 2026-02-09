db4925802d6d8fc6c557954cc8d8772f0afb5b9f
xamarsia <xamarsia@gmail.com>
2026-02-09 16:02:02 -0500

    Add scrollbar styles to dropdown and combobox.

--- packages/common-ui-components/src/combobox/MultiSelectionCombobox.tsx
+++ packages/common-ui-components/src/combobox/MultiSelectionCombobox.tsx
@@ -148,17 +148,19 @@ export default function MultiSelectionCombobox<T extends string | number>({
         </div>
 
         {isExtended && (
-          <div className="absolute w-full rounded-lg bg-white border border-stroke p-4 flex-col min-h-12 max-h-60 overflow-y-auto z-10">
-            {[...suggestedItems].map(([key, value]) => {
-              return (
-                <DropdownItem
-                  key={key}
-                  value={key}
-                  label={value}
-                  onItemSelected={onSelected}
-                />
-              );
-            })}
+          <div className="absolute w-full rounded-lg bg-white border border-stroke p-4 min-h-12 z-10">
+            <div className="w-full flex-col min-h-12 max-h-58 overflow-y-auto scrollbar">
+              {[...suggestedItems].map(([key, value]) => {
+                return (
+                  <DropdownItem
+                    key={key}
+                    value={key}
+                    label={value}
+                    onItemSelected={onSelected}
+                  />
+                );
+              })}
+            </div>
           </div>
         )}
       </div>
