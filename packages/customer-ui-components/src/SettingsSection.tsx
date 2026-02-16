1a1e8c534d41922fdbe74d7b2c6f6ee52fcd286f
xamarsia <xamarsia@gmail.com>
2026-02-15 19:37:23 -0500

    fix

--- packages/customer-ui-components/src/SettingsSection.tsx
+++ packages/customer-ui-components/src/SettingsSection.tsx
@@ -18,7 +18,7 @@ export default function SettingsSection({
       <h3>{title}</h3>
       <div className="flex grow justify-between items-start gap-6 md:gap-16 flex-col md:flex-row">
         <p className="flex w-full md:w-7/9">{description}</p>
-        <div className="w-2/5 md:w-2/9 justify-center shrink-0">{children}</div>
+        <div className="flex w-2/5 md:w-2/9 justify-center shrink-0">{children}</div>
       </div>
     </div>
   );
