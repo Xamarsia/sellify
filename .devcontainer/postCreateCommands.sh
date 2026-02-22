89f8c0d406a22950efa19fea43787e6322231ca1
xamarsia <xamarsia@gmail.com>
2026-02-21 23:58:26 -0500

    [SEL-183] Update Chrome installation in .devcontainer

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-pnpm install
+# pnpm install
+
+cd tests/e2e/venv
 
 # Install selenium in Python Virtual Environment
 python3 -m venv venv-selenium
-source ./../venv-selenium/bin/activate
+source venv-selenium/bin/activate
 
-pip install -U selenium
+pip install -U selenium==3.8.0
 # selenium --version
 
 pip install -U pytest
