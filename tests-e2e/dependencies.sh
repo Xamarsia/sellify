7cfa399c835d6ddd3fe25e7a92c07bcc5dc60a60
Ivanka A. <67323429+Xamarsia@users.noreply.github.com>
2026-02-23 23:58:52 -0500

    [SEL-186] Add force environment setup flag for e2e tests

--- tests-e2e/dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install selenium in Python Virtual Environment
-python3 -m venv venv-selenium
-. ./venv-selenium/bin/activate
-
-pip install -r ./requirements.txt
---
+++ tests-e2e/envsetup.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Function to display script usage
+
+print_usage() {
+    echo "
+Runs e2e tests environment setup.
+
+Options:
+    -h, --help      Display help message
+    -f, --force     Force venv rebuild (remove existing venv)
+"
+exit 2
+}
+
+parse_args() {
+    SHORT_OPTS="hf"
+    LONG_OPTS="help,force"
+
+    PARSED_ARGS=$(getopt -n init --options $SHORT_OPTS --longoptions $LONG_OPTS -- "$@")
+    VALID_ARGS=$?
+
+    # Print getopt's Invalid output if it is unseccessful
+    if [ "$VALID_ARGS" != "0" ]; then
+        echo "It is Invalid option: $1"
+        exit 2
+    fi
+
+    eval set -- "$PARSED_ARGS"
+    while :; do
+        case "$1" in
+        -h | --help)
+            print_usage
+            shift
+            ;;
+        -f | --force)
+            ARG_NAME=$2
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "It is Unexpected option: $1"
+            print_usage
+            ;;
+        esac
+    done
+}
+
+################################################################################
+# functions
+################################################################################
+
+setup_selenium_venv() {
+
+if [ -f ]; then
+    echo "Virtual environment forced rebuild."
+    rm -rf venv-selenium
+fi
+
+echo "Install Selenium in Python Virtual Environment"
+python3 -m venv venv-selenium
+. ./venv-selenium/bin/activate ## run this command with source instead of dot (.) to run venv
+
+echo "Installing requirements..."
+pip install -r ./requirements.txt
+
+echo "Setup complete. To activate the venv in a new shell run: \\n source ./envsetup.sh"
+}
+
+################################################################################
+# main
+################################################################################
+
+parse_args $@
+
+setup_selenium_venv
